848d7d131a60a9fbc7a19ff82da15b7ce04a7926
Arka Sheikh <skarka90@gmail.com>
2023-12-19 18:23:35 +0530

    Done upto model creation

---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,39 @@
+import { Schema, model, models } from "mongoose";
+
+const userSchema = new Schema({
+  clerkId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  usernane: {
+    type: String,
+    required: true,
+  },
+
+  firstname: {
+    type: String,
+    required: true,
+  },
+
+  lastname: {
+    type: String,
+    required: true,
+  },
+
+  photo: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = models.User || model("User", userSchema);
+
+export default User;
